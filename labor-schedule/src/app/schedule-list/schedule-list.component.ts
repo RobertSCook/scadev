2e3bee03555bd26be6a3b01bd868a2fff164e712
Cook <Robert.Cook@scasurgery.com>
2018-04-25 16:05:42 -0500

    Added Moment, moved to dynamic columns, added date to column headers

--- labor-schedule/src/app/schedule-list/schedule-list.component.ts
+++ labor-schedule/src/app/schedule-list/schedule-list.component.ts
@@ -3,6 +3,7 @@ import { TeammateSchedule } from '../models/teammate-schedule';
 import { DataService } from '../data.service';
 import { Facility } from '../models/facility';
 import { TeammateScheduleResponse } from '../models/teammate-schedule-response';
+import * as moment from 'moment';
 
 @Component({
   selector: 'sca-schedule-list',
@@ -11,7 +12,9 @@ import { TeammateScheduleResponse } from '../models/teammate-schedule-response';
 })
 export class ScheduleListComponent implements OnInit {
   scheduleList: TeammateSchedule[];
+  columnHeaders: string[];
   stringWeek: string;
+  loadingList = false;
 
   constructor(private dataService: DataService) { }
 
@@ -19,12 +22,42 @@ export class ScheduleListComponent implements OnInit {
   }
 
   getTeammateScheduleList(facility: Facility, weekStart: Date) {
+    this.loadingList = true;
     this.stringWeek = `${weekStart.getFullYear}-${weekStart.getMonth}-${weekStart.getDay}`;
     this.dataService.getTeammateSchedule(facility.facilityId, this.stringWeek).subscribe((response: TeammateScheduleResponse) => {
       this.scheduleList = response.data;
+      this.writeColumns(weekStart);
     }, (errorMsg: string) => {
       alert(errorMsg); // TODO: Change this to a toaster or something
     });
+
+
+    this.loadingList = false;
+  }
+
+  writeColumns(weekStart: Date) {
+    const columnTemplate: string[] = [
+        'Teammate',
+        'Employee Type',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+        'Sunday'];
+
+    let i = 2;
+    let columnString = '';
+    let dateContainer: moment.Moment = moment(weekStart);
+    this.columnHeaders = columnTemplate;
+
+    while (i < columnTemplate.length) {
+      dateContainer = moment(weekStart);
+      columnString = `${columnTemplate[i]} ${dateContainer.add((i - 2), 'days').format('MM/DD/YYYY')}`;
+      this.columnHeaders[i] = columnString;
+      i++;
+    }
   }
 
 }
